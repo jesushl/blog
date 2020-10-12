e6cadcaaced869b73e65dc591bd7d6824290fc85
jesushledon@gmail.com <jesushledon@gmail.com>
2020-10-12 15:47:01 -0500

    Static root

--- j3su5pro/settings.py
+++ j3su5pro/settings.py
@@ -128,6 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = '/home/j3su5/STATIC/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
