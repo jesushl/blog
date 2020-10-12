7364b183d74b7bc7e7d48f6346b8e6bac72a1511
jesushledon@gmail.com <jesushledon@gmail.com>
2020-10-12 15:43:22 -0500

    static  url patterns

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -8,4 +9,4 @@ urlpatterns = [
     path('article/<int:article_id>/', views.article, name='article'),
     path('blog/<int:page>/',views.blog, name='blog' ),
     path('me', views.me, name='me')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
