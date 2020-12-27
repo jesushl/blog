7ee21e4a2b8c07e31cae55ef466655584e1d24e4
jesushledon@gmail.com <jesushledon@gmail.com>
2020-12-27 16:35:39 -0600

    Returning to debug to fix heroku deploy error

--- j3su5pro/settings.py
+++ j3su5pro/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1$j2l#m&a59eb9c$+)fujtfbr+qbjb#ggn!$03u6($npvx6xua'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "secret-river-26786.herokuapp.com",
