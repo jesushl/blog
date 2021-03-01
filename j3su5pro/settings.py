17bfd25e514184c0548e43bf85c1994da9381fa5
jesushledon@gmail.com <jesushledon@gmail.com>
2021-02-28 21:17:25 -0600

    Fixing basedir issue

--- j3su5pro/settings.py
+++ j3su5pro/settings.py
@@ -15,7 +15,7 @@ import os
 # Heroku
 import django_heroku
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,7 +91,8 @@ WSGI_APPLICATION = 'j3su5pro.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':  'db.sqlite3',
+        # 'CONN_MAX_AGE': 500
     }
 }
 
