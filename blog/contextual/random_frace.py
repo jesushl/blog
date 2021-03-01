6785797d4e4a9163e6c4be1aa7206cbf2586cc57
jesushledon@gmail.com <jesushledon@gmail.com>
2021-02-28 21:29:55 -0600

    Adding a patch for operational issue

--- blog/contextual/random_frace.py
+++ blog/contextual/random_frace.py
@@ -4,6 +4,9 @@ import random as ra
 from blog.models import Frace
 
 def get_random_frase():
-    number_of_many_frases = Frace.objects.all().count()
-    frace_index = ra.randrange(0, number_of_many_frases)
-    return Frace.objects.all()[frace_index].frace
+    try:
+        number_of_many_frases = Frace.objects.all().count()
+        frace_index = ra.randrange(0, number_of_many_frases)
+        return Frace.objects.all()[frace_index].frace
+    except OperationalError:
+        return ''
