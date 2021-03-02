80e14dcf90e6cb6a3f2c60aacb8dd92325d6ee0b
jesushledon@gmail.com <jesushledon@gmail.com>
2021-03-01 22:14:32 -0600

    Adding technology into admin contrib

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Article, JobExperience
 from .models import Frace, Message, ContactCard, Image
+from .models import Technology
 
 admin.site.register(Article)
 admin.site.register(JobExperience)
@@ -10,3 +11,4 @@ admin.site.register(Frace)
 admin.site.register(Message)
 admin.site.register(ContactCard)
 admin.site.register(Image)
+admin.site.register(Technology)
