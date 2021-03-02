8325e795ee1adf93441f9eaa7cd783fd885343f9
jesushledon@gmail.com <jesushledon@gmail.com>
2021-03-01 21:14:22 -0600

    Fixing an error on random frace

--- blog/contextual/random_frace.py
+++ blog/contextual/random_frace.py
@@ -3,8 +3,11 @@ import random as ra
 # Models
 from blog.models import Frace
 
+
 def get_random_frase():
-    
     number_of_many_frases = Frace.objects.all().count()
     frace_index = ra.randrange(0, number_of_many_frases)
-    return Frace.objects.all()[frace_index].frace
+    if number_of_many_frases:
+        return Frace.objects.all()[frace_index].frace
+    else:
+        return ''
