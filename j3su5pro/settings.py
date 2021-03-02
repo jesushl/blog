863ce13b6d6a436bc659afc03f5a23fd729919f1
jesushledon@gmail.com <jesushledon@gmail.com>
2021-03-01 20:45:59 -0600

    Adding heroku values for static files

--- j3su5pro/settings.py
+++ j3su5pro/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
-#Python
+# Python
 import os
 # Heroku
 import django_heroku
@@ -109,16 +109,28 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
     },
 ]
 
@@ -141,11 +153,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/tmp/www/j3su5pro/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+# CKeditor
 CKEDITOR_UPLOAD_PATH = '/static/'
-
 CKEDITOR_CONFIGS = {
     'default': {
         'toolbar': 'full',
@@ -153,6 +170,5 @@ CKEDITOR_CONFIGS = {
         'width': '75%',
     },
 }
-
-DATABASES={}
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
