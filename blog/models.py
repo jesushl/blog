276bc2175583e8781bed37528835fd1769a833ed
jesushledon@gmail.com <jesushledon@gmail.com>
2021-04-06 20:06:13 -0500

    Actualizing to a real value for twitter

--- blog/models.py
+++ blog/models.py
@@ -99,7 +99,7 @@ class ContactCard(models.Model):
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     adress = models.CharField(max_length=100, null=True, blank=True)
     skype = models.CharField(max_length=20, null=True, blank=True)
-    twitter = models.CharField(max_length=10, null=True, blank=True)
+    twitter = models.CharField(max_length=30, null=True, blank=True)
     linkedin = models.CharField(max_length=70, null=True, blank=True)
     image = models.ForeignKey(
         Image,
